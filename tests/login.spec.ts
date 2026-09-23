ceb5c3850e8ec8c978b0b9dd009300a07363a458
Pablo Soares Lopes <mpablosl@gmail.com>
2026-09-23 19:55:31 -0300

    Caso de teste TC-LOGIN-004 adicionado

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -32,3 +32,13 @@ test("TC-LOGIN-003 - Não deve realizar login com senha inválida", async ({page
 
     await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
 });
+
+test("TC-LOGIN-004 - Não deve realizar login com usuário inválido", async ({page}) => {
+    await page.goto('/');
+
+    await page.locator('[data-test="username"]').fill('invalid_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
+});
